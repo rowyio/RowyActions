dfff6d37d5cf865c0281269ae39afdcc4120ebb7
Shams mosowi <shams.mosowi@gmail.com>
2021-02-20 13:32:02 +0800

    callable action wrapper

---
+++ functions/src/callableWrapper.ts
@@ -0,0 +1,41 @@
+import * as functions from "firebase-functions";
+import {db} from "./firebaseConfig";
+import validateAction from "./actionValidation";
+type ActionData = {
+    ref: {
+      id: string;
+      path: string;
+      parentId: string;
+      tablePath: string;
+    };
+    schemaDocPath: string;// docPath of
+    column: {key: string};
+    action: "run" | "redo" | "undo";
+  };
+
+const callableActionWrapper=(
+    actionScript:(args:{callableData:ActionData, context: functions.https.CallableContext, row:any})=>
+        Promise<{success:boolean, message:string}>) =>
+  functions.https.onCall(async (callableData: ActionData, context: functions.https.CallableContext) => {
+    try {
+      const {ref, column, schemaDocPath} = callableData;
+      // fetch the row and the table schema snapshots
+      const [schemaSnapshot, rowSnapshot] = await Promise.all([
+        db.doc(schemaDocPath).get(),
+        db.doc(ref.path).get(),
+      ]);
+      const row = rowSnapshot.data();
+      // preforms validation of the required conditions for action to run
+      validateAction({context, row, schemaSnapshot, column});
+      const result = await actionScript({callableData, context, row});
+      return result;
+    } catch (error) {
+      return {
+        success: false,
+        error,
+        message: error.message,
+      };
+    }
+  });
+
+export default callableActionWrapper;
