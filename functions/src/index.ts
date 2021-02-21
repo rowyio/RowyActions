678a3fc972d24d336f840c5314f94b11f00a1a88
Shams mosowi <shams.mosowi@gmail.com>
2021-02-21 21:38:46 +0800

    documentation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,14 @@
 import callableAction from "./callableAction";
-export const wrappedAction = callableAction(() =>{
+export const CallableAction = callableAction(({row,callableData,context}) =>{
+  const {ref, column, schemaDocPath,action} = callableData;
+  console.log({
+    row, // docSnapshot of the called column
+    context, // callable context contains data such as the time and the user running the action
+    ref, // contains document id and other references related to the row
+    column, // contains key of the action column
+    schemaDocPath, // table schema Doc Path
+    action // latest action state  
+  })
   return {success: true, message: "hello world"};
 });
 
