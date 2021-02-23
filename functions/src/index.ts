b896fb8bc75d21f6c8a34ced04fc01f268ba6b9a
Shams mosowi <shams.mosowi@gmail.com>
2021-02-23 10:51:41 +0800

    update demo

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,14 +9,14 @@ const auth = admin.auth()
 
 export const SendUserInvite = callableAction(async ({row, callableData}) =>{
   const {action} = callableData;
-  const {firstName,lastName,email,roles} =row
+  const {firstName,lastName,email,roles,uid} =row
   // switch statement can be used to perform different event based on the state of the action cell
   switch (action) {
     case "run":
     // create firebase user    
     auth.importUsers([
       {
-        uid: 'some-uid',
+        uid,
         displayName: `${firstName} ${lastName}`,
         email,
         // set users roles
@@ -47,21 +47,22 @@ export const SendUserInvite = callableAction(async ({row, callableData}) =>{
 
 export const suspendUser = callableAction(async ({row, callableData}) =>{
   const {action} = callableData;
+  const {firstName,uid} = row
   // switch statement can be used to perform different event based on the state of the action cell
   switch (action) {
     case "run":
     case "redo":
       // both run and redo preform the same action; disabling the user's account from firebase auth
-      auth.updateUser(row.uid, {disabled:true})
+      auth.updateUser(uid, {disabled:true})
       return {success: true, // return if the operation was success
-        message: `${row.firstName}'s account has been disabled`, // message shown in snackbar on the firetable ui after the completion of action
+        message: `${firstName}'s account has been disabled`, // message shown in snackbar on the firetable ui after the completion of action
         cellStatus: "disabled", // optional cell label, to indicate the latest state of the cell/row
         newState: "undo", // "redo" | "undo" | "disabled" are options set the behavior of action button next time it runs
       };
     case "undo":
-      auth.updateUser(row.uid, {disabled:true})
+      auth.updateUser(uid, {disabled:true})
       return {success: true, // return if the operation was success
-        message: `${row.firstName}'s account has been re-enabled`, // message shown in snackbar on the firetable ui after the completion of action
+        message: `${firstName}'s account has been re-enabled`, // message shown in snackbar on the firetable ui after the completion of action
         cellStatus: "invited", // optional cell label, to indicate the latest state of the cell/row
         newState: "redo", // "redo" | "undo" | "disabled" are options set the behavior of action button next time it runs
       };
