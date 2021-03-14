b84ff98337b7a64f61527d511ef214d4df13ef3f
Shams mosowi <shams.mosowi@gmail.com>
2021-03-14 15:14:46 +1000

    fix update user roles

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
 
 export {SendUserInvite} from "./actions/SendUserInvite";
-export {updateUserRoles} from "./actions/updateUserRoles";
 export {SuspendUser} from "./actions/SuspendUser";
+export {UpdateUserRoles} from "./actions/UpdateUserRoles";
