6929629a782e954659276a9a0d14ee0a398e6f4b
Shams mosowi <shams.mosowi@gmail.com>
2021-03-15 16:47:36 +1000

    suspend user, to use email

--- functions/src/actions/SuspendUser.ts
+++ functions/src/actions/SuspendUser.ts
@@ -1,14 +1,18 @@
-import {auth} from "../firebaseConfig";
+
+import * as admin from "firebase-admin";
+const auth = admin.auth();
+
 import callableAction from "firetable-actions";
 export const SuspendUser = callableAction(async ({row, callableData}) =>{
   const {action} = callableData;
-  const {firstName, uid} = row;
+  const {firstName, email} = row;
   // switch statement can be used to perform different event based on the state of the action cell
+  const user = await auth.getUserByEmail(email);
   switch (action) {
     case "run":
     case "redo":
       // both run and redo preform the same action; disabling the user's account from firebase auth
-      await auth.updateUser(uid, {disabled: true});
+      await auth.updateUser(user.uid, {disabled: true});
       return {success: true, // return if the operation was success
         message: `${firstName}'s account has been disabled`, // message shown in snackbar on the firetable ui after the completion of action
         cellStatus: "disabled", // optional cell label, to indicate the latest state of the cell/row
@@ -16,7 +20,7 @@ export const SuspendUser = callableAction(async ({row, callableData}) =>{
       };
     case "undo":
       // re-enable user's firebase account
-      await auth.updateUser(uid, {disabled: false});
+      await auth.updateUser(user.uid, {disabled: false});
       return {success: true, // return if the operation was success
         message: `${firstName}'s account has been re-enabled`, // message shown in snackbar on the firetable ui after the completion of action
         cellStatus: "active", // optional cell label, to indicate the latest state of the cell/row
